03222fc7675927f4f60c9ceb332bef77fbbe23a9
pazzi <carlos.pazzianotto@gmail.com>
2020-10-24 16:15:50 -0300

    Insercao do stdlib.h e do system("clear")

--- estrutura_dados1/exercicios/ex01.c
+++ estrutura_dados1/exercicios/ex01.c
@@ -1,12 +1,14 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+    system("clear");
     float base, altura;
     printf("\nInforme o valor da base do retângulo: ");
     scanf("%f", &base);
     printf("\nInforme o valor da altura do retângulo: ");
     scanf("%f", &altura);
-    printf("\nO valor da área do retângulo é:%.2f", base*altura);
+    printf("\nO valor da área do retângulo é:%.2f\n\n", base*altura);
 
 }
