e8ec5c76e83b5ebfbc44b946dbd3aded69e1d0b8
pazzi <carlos.pazzianotto@gmail.com>
2020-10-24 16:31:48 -0300

    Alterações no ex02.c

--- estrutura_dados1/exercicios/ex02.c
+++ estrutura_dados1/exercicios/ex02.c
@@ -1,16 +1,17 @@
-
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     float bruto, desconto;
     int dias;
+    system("clear");
     printf("\nInforme a quantidade de dias trabalhados: ");
     scanf("%i", &dias);
     bruto = 60 * dias;
     desconto = bruto * 0.11;
-    printf("\nO trabalhador trabalhou %i: ", dias);
-    printf("\nO  valor bruto a receber é %f: ", bruto);
-    printf("\nO desconto de IR é %f: ", desconto);
-    printf("\nO valor líquido a receber é %f: ", bruto - desconto);
+    printf("\nO trabalhador trabalhou %i dias", dias);
+    printf("\nO valor bruto a receber é R$ %.2f", bruto);
+    printf("\nO desconto de IR é R$ %.2f", desconto);
+    printf("\nO valor líquido a receber é R$ %.2f\n\n: ", bruto - desconto);
 
 }
