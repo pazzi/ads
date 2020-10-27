e3a2ba680bb86760be1974ea0e9cd5fb2bb29c9f
pazzi <carlos.pazzianotto@gmail.com>
2020-10-27 15:40:08 -0300

    Inserção do ex07.c

---
+++ estrutura_dados1/exercicios/ex07.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int timeCasa, timeAdversario;
+    system("clear");
+    printf("\nInforme os gols marcados pela equipe da casa e pelo adversário ");
+    scanf("%i %i", &timeCasa, &timeAdversario);
+    if (timeCasa == timeAdversario)
+        printf("\nEMPATE\n");
+    if (timeCasa > timeAdversario)
+    {
+        printf("\nVITÓRIA DO TIME DA CASA\n\n");
+    }
+    else 
+    {
+        printf("\nVITÓRIA DA EQUIPE VISITANTE\n\n");
+        }
+}
