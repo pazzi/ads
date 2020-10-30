dcc22fc16fd01bfec9ac6d264e2dcf4af702f12a
pazzi <carlos.pazzianotto@gmail.com>
2020-10-30 15:15:08 -0300

    Insercao do ex12.c e da linha fflush(stdio) nos codigos

---
+++ estrutura_dados1/exercicios/ex12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    ///Declaracao de variaveis
+    int umNumero, outroNumero;
+    ///Entrada de dados
+    //processamento
+    for(umNumero = 1; umNumero <= 10; umNumero++)
+    {
+        for(outroNumero = 1; outroNumero <=10; outroNumero++)    
+        { 
+           printf("\n%2i x %2i =  %2i",
+                   umnumero, outroNumero, umNumero * outroNumero)
+        } 
+    
+    }
+
+}
