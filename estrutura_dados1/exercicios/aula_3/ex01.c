3c695573816fab7da85464459c488f34139a2ac9
pazzi <carlos.pazzianotto@gmail.com>
2020-11-05 17:28:37 -0300

    inseridos ex01.c e ex02.c da aula 3.

--- estrutura_dados1/exercicios/aula_3/ex01.c
+++ estrutura_dados1/exercicios/aula_3/ex01.c
@@ -3,4 +3,21 @@ imprima o número da tabela ASCII que corresponde ao ‘A’ e depois o valor he
 que que equivale ao ‘A’ . Faz a mesma coisa para o caractere ‘a’ .
 */
 
+#include <stdio.h>
+void mostra_caracter(char letra)
+{
+	printf("Impressões do caracter %c\n\n", letra);
+	printf("O caracter pp dito:%c\n",letra);
+	printf("O correspondente decimal:%d\n",letra);
+	printf("O correspondente em octal:%o\n",letra);
+	printf("O correspondente em hexadecimal:%X\n\n",letra);
+}
+int main()
+{
+	char valor;
+	valor='A';
+	mostra_caracter(valor);
+	valor='a';
+	mostra_caracter(valor);
+}
 
--- estrutura_dados1/exercicios/aula_3/ex02.c
+++ estrutura_dados1/exercicios/aula_3/ex02.c
@@ -5,3 +5,18 @@ int b ;
 float c ;
 double d ;
 */
+#include <stdio.h>
+
+int main()
+{
+	char a;
+	int b;
+	float c;
+	double d;
+	printf("Quantidade de bytes em memória ocupados por variáveis:\n");
+	printf("Variáveis char  - %ld bytes\n",sizeof(a));
+	printf("Variáveis int   - %ld bytes \n",sizeof(b));
+	printf("Variáveis float - %ld bytes\n",sizeof(c));
+	printf("Variáveis double- %ld bytes\n",sizeof(d));
+	return 0;
+}
