1e594253808916566b9c0c6ce316738e25ef6520
pazzi <carlos.pazzianotto@gmail.com>
2020-12-05 21:44:39 -0300

    Alteracoes no contador for e na inclusao de indicePositivo e indiceNegativo

--- estrutura_dados1/exercicios/aula_4_5_vetores/ex07.c
+++ estrutura_dados1/exercicios/aula_4_5_vetores/ex07.c
@@ -3,46 +3,40 @@
 
 int main(){
 	system("clear");
-	int mtx1[12];
-	int mtx2[12];
-	int mtx3[12];
-	int vl,i,j,m,n=0;
-	for (i=0; i <= 11; i++)
+	int mtx1[12], mtx2[12], mtx3[12];
+	int i,indicePositivo, indiceNegativo=0;
+	for (i=0; i < 12; i++)
 	{
 		printf("\nInforme o valor %i de 12 posição %i do array de doze: ",i+1, i);
-		scanf(" %i",&vl);
-		mtx1[i]=vl;
+		scanf(" %i",&mtx1[i]);
 	}
 
 printf("\n\n");
 
-	for (j=0; j <= 11; j++)
+	for (i=0; i < 12; i++)
 	{
-		if (mtx1[j] > 0)
+		if (mtx1[j] >= 0)
 		{
-			mtx2[j]=mtx1[j];
-			mtx3[j]=0;
-			m++;
+			mtx2[indicePositivo]=mtx1[j];
+			indicePositivo++;
 
 		}else{
-			mtx3[j]=mtx1[j];
-			mtx2[j]=0;
-			n++;
+			mtx3[indiceNegativo]=mtx1[j];
+			indiceNegativo++;
 		}
-
 	}
 
 printf("\n\n");
 printf("O array 1 terá %i valores: ", m);
 
-	for (i=0; i <= 11; i++)
+	for (i=0; i < indicePositivo; i++)
 	{
 		if (mtx2[i] != 0)
 			printf("%i ",  mtx2[i]);
 	}
 printf("\n\n");
 printf("O array 2 terá %i valores: ", n);
-	for (i=0; i <= 11; i++)
+	for (i=0; i < indiceNegativo; i++)
 	{
 		if (mtx3[i] != 0)
 			printf("%i ",  mtx3[i]);
