a5579b05c7995bd2d3e05324ef27376748e20a66
pazzi <carlos.pazzianotto@gmail.com>
2020-12-05 21:20:28 -0300

    Alterando o scanf do ex03

--- estrutura_dados1/exercicios/aula_4_5_vetores/ex03.c
+++ estrutura_dados1/exercicios/aula_4_5_vetores/ex03.c
@@ -5,24 +5,24 @@ int main(){
 	system("clear");
 	int mtx[10];
 	int i, vl;
-	for (i=0; i <= 9; i++)
+	for (i=0; i < 10; i++)
 	{
 		if(i == 0)
 		{
 			printf("\nInforme o valor inicial positivo da matriz: ");
-			scanf(" %i",&vl);
+			scanf(" %i",&mtx[i]);
 			mtx[i]=vl;
 		}
 		else{
 			if (i % 2 == 0)
 			{
-				mtx[i] = vl;
+				mtx[i] = mtx[0];
 			}else{
-				mtx[i] = -vl;
+				mtx[i] = -mtx[0];
 			}
 		}
 	}
-	for (i=0; i <= 9; i++)
+	for (i=0; i < 10; i++)
 	{
 		printf("\n A matriz na posicao %i é :%i",  i, mtx[i]);
 	}
