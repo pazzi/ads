35bdc67366045ab18e283c1508a1742fc6c88dc3
pazzi <carlos.pazzianotto@gmail.com>
2020-12-05 21:25:22 -0300

    Alterado o contador do for e o scanf do ex04.c

--- estrutura_dados1/exercicios/aula_4_5_vetores/ex04.c
+++ estrutura_dados1/exercicios/aula_4_5_vetores/ex04.c
@@ -7,20 +7,18 @@ int main(){
 	int mtx2[10];
 	int mtx3[10];
 	int i, vl;
-	for (i=0; i <= 9; i++)
+	for (i=0; i < 10; i++)
 	{
 		printf("\nInforme o valor %i de 10 para o array 1: ",i+1);
-		scanf(" %i",&vl);
-		mtx1[i]=vl;
+		scanf(" %i",&mtx1[i]);
 	}
 
 printf("\n\n");
 
-	for (i=0; i <= 9; i++)
+	for (i=0; i < 10; i++)
 	{
 		printf("\nInforme o valor %i de 10 para o array 2: ",i+1);
-		scanf(" %i",&vl);
-		mtx2[i]=vl;
+		scanf(" %i",&mtx2[i]);
 	}
 	for (i=0; i <= 9; i++)
 	{
@@ -34,7 +32,7 @@ printf("\n\n");
 
 printf("\n\n");
 
-	for (i=0; i <= 9; i++)
+	for (i=0; i < 10; i++)
 	{
 		printf("\n O valor do array 3[%i] é :%i ",  i, mtx3[i]);
 	}
