8f1c490a78fa38ba8b984129da7b7e982e5ad2a3
pazzi <carlos.pazzianotto@gmail.com>
2020-12-05 21:32:22 -0300

    Acerto do contador do for e o scanf do ex06.c

--- estrutura_dados1/exercicios/aula_4_5_vetores/ex06.c
+++ estrutura_dados1/exercicios/aula_4_5_vetores/ex06.c
@@ -5,20 +5,19 @@ int main(){
 	system("clear");
 	int mtx1[12];
 	int mtx[3][4];
-	int vl,i,j,k,l=0;
+	int i,j,k,l=0;
 	for (i=0; i <= 11; i++)
 	{
 		printf("\nInforme o valor %i de 12 posição %i do array de doze: ",i+1, i);
-		scanf(" %i",&vl);
-		mtx1[i]=vl;
+		scanf(" %i",&mtx1[i]);
 	}
 
 printf("\n\n");
 
-	for (j=0; j <= 2; j++)
+	for (j=0; j < 3; j++)
 	{
 		l=j;
-		for (k=0; k <= 3; k++ )
+		for (k=0; k < 4; k++ )
 		{
 			mtx[j][k]=mtx1[l];
 			l+=3;
@@ -27,10 +26,10 @@ printf("\n\n");
 
 printf("\n\n");
 
-	for (i=0; i <= 2; i++)
+	for (i=0; i < 3; i++)
 	{
 		printf("\n Os valores do array %i sao :",  i+1);
-		for ( j=0; j <=3; j++ )
+		for ( j=0; j < 4; j++ )
 		{
 			printf("%i ", mtx[i][j]);
 		}
